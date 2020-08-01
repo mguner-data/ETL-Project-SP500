50c89c6f46af5eaee4bdfc91f0a0e63eda8de9a5
weiliann77 <weilian77@gmail.com>
2020-08-01 10:28:55 -0500

    updated schema

--- Schema.sql
+++ Schema.sql
@@ -1,47 +1,33 @@
-create table Ticker_Security_Name (
-	Ticker text primary key,
-	Security_Name text
+create table Ticker_Security (
+	ticker text primary key,
+	Security_Name text,
+	SP500 boolean,
+	DJIa30 boolean
 );
 
-create table Add_Sub(
+create table Index_Event(
 	Index int primary key,
-	Reason text
+	ticker text,
+	date_close date,
+	reason text,
+	direction boolean,
+	Indices text,
+	foreign key (ticker)
+	references Ticker_Security(ticker)
 );
 
-create table Addition(
-	Add_Sub_ID int ,
-	Ticker text,
-	foreign key (Add_Sub_ID)
-	references Add_Sub(ID)
-);
-
-create table Subtraction(
-	Add_Sub_ID int ,
-	Ticker text,
-	foreign key (Add_Sub_ID)
-	references Add_Sub(ID)
-);
-
-
-
-create table Curren_Component(
-	Ticker text,
-	Date_Added date,
-	foreign key (Ticker)
-	references Ticker_Security_Name(Ticker)
-);
 
 create table Daily_Data(
 	Index int primary key,
 	Ticker text,
-	Date_Close date,
+	date_close date,
 	Open float,
 	High float,
 	Low float,
 	Close float,
 	Volume int,
 	foreign key (Ticker)
-	references Ticker_Security_Name(Ticker)
+	references Ticker_Security(ticker)
 );
 
 
